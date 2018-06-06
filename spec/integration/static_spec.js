0fabe244b9e537f165d1bab2deb6f6483fb1f1e6
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-06-06 09:03:38 -0700

    still trying

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:5000/";
+const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
     describe("GET /", () => {
