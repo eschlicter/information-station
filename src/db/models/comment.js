4cd2421ef88dc8c67990f777cef7cdb73182563d
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-06-27 13:13:58 -0700

    checkpoint public profiles

--- src/db/models/comment.js
+++ src/db/models/comment.js
@@ -1,30 +1,42 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Comment = sequelize.define('Comment', {
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    postId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
+   body: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   postId: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+   },
+   userId: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+   }
   }, {});
   Comment.associate = function(models) {
-    // associations can be defined here
     Comment.belongsTo(models.Post, {
-      foreignKey: "postId",
-      onDelete: "CASCADE"
-    });
+       foreignKey: "postId",
+       onDelete: "CASCADE"
+     });
+     Comment.belongsTo(models.User, {
+       foreignKey: "userId",
+       onDelete: "CASCADE"
+     });
+     Comment.addScope("lastFiveFor", (userId) => {
 
-    Comment.belongsTo(models.User, {
-      foreignKey: "userId",
-      onDelete: "CASCADE"
-    });
+ // #1
+     return {
+       include: [{
+         model: models.Post
+       }],
+       where: { userId: userId},
+
+       limit: 5,
+       order: [["createdAt", "DESC"]]
+     }
+   });
   };
+  
   return Comment;
 };
