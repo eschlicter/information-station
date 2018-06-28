550e0dd8976e1f8d43c1918ae0eaaafeca72bb46
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-06-28 07:42:43 -0700

    assignment public profiles complete

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,48 +1,59 @@
 const User = require("./models").User;
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
+const Favorite = require("./models").Favorite;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-    createUser(newUser, callback){
-        const salt = bcrypt.genSaltSync();
-        const hashedPassword = bcrypt.hashSync(newUser.password, salt);
 
-        return User.create({
-            email: newUser.email, 
-            password: hashedPassword
-        })
-        .then((user) => {
-            callback(null, user);
-        })
-        .catch((err) => {
+  createUser(newUser, callback){
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+    return User.create({
+      email: newUser.email,
+      password: hashedPassword
+    })
+    .then((user) => {
+      callback(null, user);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  getUser(id, callback){
+    let result = {};
+    User.findById(id)
+    .then((user) => {
+      if(!user) {
+        callback(404);
+      } else {
+        result["user"] = user;
+
+        Post.scope({method: ["lastFiveFor", id]}).all()
+        .then((posts) => {
+          result["posts"] = posts;
+
+          Comment.scope({method: ["lastFiveFor", id]}).all()
+          .then((comments) => {
+            result["comments"] = comments;
+
+            Favorite.scope({method: ["favoritePosts", id]}).all()
+            .then((favorites) => {
+              result["favorites"] = favorites;
+
+              callback(null, result);
+            })
+            .catch((err) => {
+              callback(err);
+            })
+          })
+          .catch((err) => {
             callback(err);
+          })
         })
-    },
-    getUser(id, callback){
-           let result = {};
-           User.findById(id)
-           .then((user) => {
-             if(!user) {
-               callback(404);
-             } else {
+      }
+    })
+  }
 
-               result["user"] = user;
-
-               Post.scope({method: ["lastFiveFor", id]}).all()
-               .then((posts) => {
-
-                 result["posts"] = posts;
-                 Comment.scope({method: ["lastFiveFor", id]}).all()
-                 .then((comments) => {
-                   result["comments"] = comments;
-                   callback(null, result);
-                 })
-                 .catch((err) => {
-                   callback(err);
-                 })
-               })
-             }
-           })
-         }
 }
